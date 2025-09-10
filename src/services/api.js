993473ed3cbfb882ddc13d376eb16d5141be0990
ishaantaneja <tanejaishaan3@gmail.com>
2025-09-10 18:41:37 +0530

    backend functionality is all online now

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "https://portfolio-web-backend-02i8.onrender.com"; // backend running on render.com
+const API_BASE = import.meta.env.VITE_API_URL; // backend running on render.com
 
 // Projects - only fetching is public
 export const fetchProjects = () => axios.get(`${API_BASE}/projects`);
