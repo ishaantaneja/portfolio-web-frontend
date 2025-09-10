f0dc816a89bd095c6b934d2d57c929a564815e93
ishaantaneja <tanejaishaan3@gmail.com>
2025-09-10 17:57:10 +0530

    added backend url from render for web hosting

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:5000/api"; // backend running locally
+const API_BASE = "https://portfolio-web-backend-02i8.onrender.com/"; // backend running locally
 
 // Projects - only fetching is public
 export const fetchProjects = () => axios.get(`${API_BASE}/projects`);
